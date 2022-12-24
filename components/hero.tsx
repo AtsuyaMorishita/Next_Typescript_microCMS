f6594c67de2aa085603cda1bde28e89c8d863a4a
AtsuyaMorishita <mori.a0426@gmail.com>
2022-12-24 23:03:44 +0900

    スタイル調整

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,17 +1,36 @@
+import styled from "styled-components";
+
+//型定義
 type Props = {
   title: string;
   subTitle: string;
   imageOn: boolean;
-}
+};
 
 export const Hero = (props: Props) => {
   const { title, subTitle, imageOn } = props;
 
   return (
-    <div>
-      <h1>{ title }</h1>
-      <p>{ subTitle }</p>
-      { imageOn && <figure>[画像]</figure> }
-    </div>
-  )
-}
+    <SText>
+      <STitle>{title}</STitle>
+      <SSubTitle>{subTitle}</SSubTitle>
+      {imageOn && <figure>[画像]</figure>}
+    </SText>
+  );
+};
+
+//styled-component
+const SText = styled.div`
+  padding-top: calc(var(--display) * 0.5);
+  padding-bottom: calc(var(--display) * 0.7);
+`;
+
+const STitle = styled.h1`
+  font-size: var(--display);
+  font-weight: 900;
+  letter-spacing: 0.15em;
+`;
+
+const SSubTitle = styled.p`
+  font-size: var(--small-heading2);
+`;
--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,22 @@
-import Link from "next/link"
+import Link from "next/link";
+import styled from "styled-components";
 
-export const Logo = ()  => {
+type Props = {
+  boxStyle?: boolean;
+};
+
+export const Logo = () => {
   return (
-    <Link href="/">CUBE</Link>
-  )
-}
+    <SLogoLink href="/" boxStyle>
+      CUBE
+    </SLogoLink>
+  );
+};
+
+const SLogoLink = styled(Link)<Props>`
+  font-size: var(--heading2);
+  font-weight: 700;
+  letter-spacing: 0.15em;
+
+  color: ${({ boxStyle }) => boxStyle && "red"};
+`;
