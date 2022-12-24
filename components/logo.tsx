8442e2ca9b5cbf516f45ac281cb81f31645d94f5
AtsuyaMorishita <mori.a0426@gmail.com>
2022-12-25 00:30:21 +0900

    属性の有無によってcssを切り替える実装 コメントメモの追加

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,21 +2,38 @@ import Link from "next/link";
 import styled from "styled-components";
 
 type Props = {
-  boxStyle?: boolean;
+  /**
+   * 呼び出し元でlogoBoxがあればtrue、なければfalse
+   * logoBoxが未記載の時にエラーが出ないようにハテナをつける
+   */
+  logoBox?: boolean;
 };
 
-export const Logo = () => {
+export const Logo = (props: Props) => {
+  const { logoBox } = props;
+
+  //型定義されている場合、returnの中は定義されたオブジェクトしか使えない
   return (
-    <SLogoLink href="/" boxStyle>
-      CUBE
-    </SLogoLink>
+    <>
+      {/* {propsで受け取ったlogoBoxの値をlogoBoxへ再代入している？} */}
+      <SLogoLink href="/" logoBox={logoBox}>
+        CUBE
+      </SLogoLink>
+    </>
   );
 };
 
 const SLogoLink = styled(Link)<Props>`
+  //ロゴ共通
   font-size: var(--heading2);
   font-weight: 700;
   letter-spacing: 0.15em;
 
-  color: ${({ boxStyle }) => boxStyle && "red"};
+  //ロゴ ボックススタイルのみ
+  //logoBoxのtrueかfalseかで条件分岐
+  display: ${({ logoBox }) => logoBox && "inline-block"};
+  padding: ${({ logoBox }) => logoBox && "1em 2em"};
+  background-color: ${({ logoBox }) => logoBox && "var(--gray-75)"};
+  color: ${({ logoBox }) => logoBox && "var(--white)"};
+  font-size: ${({ logoBox }) => logoBox && "var(--small-heading2)"};
 `;
