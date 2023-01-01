d73cecc4b3a1244d9a811360bf4aa86ed3ba03c9
AtsuyaMorishita <mori.a0426@gmail.com>
2023-01-01 11:10:30 +0900

    全体横幅 レイアウト調整

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,11 +4,50 @@ import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 
 import { Hero } from "components/hero";
+import { Container } from "components/container";
+import { PostBody } from "components/postBody";
+import { Contact } from "components/contact";
+import {
+  TwoColumn,
+  TwoColumnMain,
+  TwoColumnSidebar,
+} from "components/twoColumn";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function About() {
   return (
-    <Hero title="CUBE" subTitle="こちらはaboutページです。" imageOn={false} />
+    <Container>
+      <Hero title="CUBE" subTitle="こちらはaboutページです。" imageOn={false} />
+
+      <TwoColumn>
+        <TwoColumnMain>
+          <PostBody>
+            <p>
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+            </p>
+            <h2>こちらはダミーテキストです。こちらはダミーテキストです。</h2>
+            <p>
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+            </p>s
+            <p>
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+            </p>
+            <h3>こちらはダミーテキストです。</h3>
+            <p>
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+              こちらはダミーテキストです。こちらはダミーテキストです。こちらはダミーテキストです。
+            </p>
+          </PostBody>
+        </TwoColumnMain>
+
+        <TwoColumnSidebar>
+          <Contact />
+        </TwoColumnSidebar>
+      </TwoColumn>
+    </Container>
   );
 }
