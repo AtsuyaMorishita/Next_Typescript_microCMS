a62cbc524fa52570dba6778b113b5f99e91ea434
AtsuyaMorishita <mori.a0426@gmail.com>
2023-01-15 00:14:37 +0900

    プログレスバーの実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,9 +7,19 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
+//プログレスバー(https://blog.parity-box.com/posts/diary/2021/08/25)
+import NextNprogress from "nextjs-progressbar";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <NextNprogress
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+        startPosition={0.3}
+        color="#29D"
+      />
       <Component {...pageProps} />
     </Layout>
   );
